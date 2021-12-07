4469f3c16b735e9dc418756937ef5b701f34fbe7
imadeyogabp <60782620+imadeyogabp@users.noreply.github.com>
2021-12-07 19:40:47 +0800

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_cors import CORS, cross_origin
-from router import Router
-
-app  = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-Router.run(app)
